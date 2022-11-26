eb03013e5dad278ea512280608b6037c33387766
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-11-26 20:36:26 -0300

    feat: add horizontal padding to Main component

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const MainContainer = styled.main`
-    max-width: 730px;
+    max-width: 762px;
     width: 100%;
     margin: 9rem auto 0;
+    padding: 0 2rem;
 `
