d51bdc188181042b96770c29282656bf9030c680
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-11-26 20:00:17 -0300

    feat: display theme button in Header based on current theme

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,29 @@
-import { Moon } from 'phosphor-react'
+import { Moon, Sun } from 'phosphor-react'
+import { useState } from 'react';
 import { Container } from "./styles";
 
 export function Header () {
+    const [theme, setTheme] = useState('light')
+
+    function handleSwitchThemes () {
+        const newTheme = theme === 'light' ? 'dark' : 'light'
+        setTheme(newTheme)
+    }
+
     return (
         <Container>
             <h1>devfinder</h1>
 
-            <button>
-                DARK
-                <Moon weight='fill' size={20} />
+            <button onClick={handleSwitchThemes}>
+                {theme === 'light' ? 'DARK' : 'LIGHT'}
+                {theme === 'light' ?
+                    (
+                        <Moon weight='fill' size={20} />
+                    ) :
+                    (
+                        <Sun weight='fill' size={20} />
+                    )
+                }
             </button>
         </Container>
     )
