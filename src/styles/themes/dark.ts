2c1594b79a4a7fb2b62fc6e78dabc5558706dc40
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-11-26 19:06:13 -0300

    feat: add light and dark themes

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,20 @@
+export const darkTheme = {
+    colors: {
+        active: '#0079FF',
+        activeHighlight: '#60ABFF',
+        background: '#141D2F',
+        card: '#1E2A47',
+        title: '#FFFFFF',
+        text: '##FFFFFF',
+        lightText: '#FFFFFF',
+        lightTextHover: '#90A4D4',
+        error: '#F74646'
+    },
+    fonts: {
+        '3xl': '1.625rem',
+        '2xl': '1.375rem',
+        xl: '1rem',
+        md: '0.9375rem',
+        sm: '0.8125rem'
+    }
+}
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+    colors: {
+        active: '#0079FF',
+        activeHighlight: '#60ABFF',
+        background: '#F6F8FF',
+        card: '#FEFEFE',
+        title: '#2B3442',
+        text: '#4B6A9B',
+        lightText: '#697C9A',
+        lightTextHover: '#222731',
+        error: '#F74646'
+    },
+    fonts: {
+        '3xl': '1.625rem',
+        '2xl': '1.375rem',
+        xl: '1rem',
+        md: '0.9375rem',
+        sm: '0.8125rem'
+    }
+}
