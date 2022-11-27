e6069712313b886873fb33c0e2807db9f7f9c23a
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-11-27 14:27:03 -0300

    feat: decrease top and bottom margins of Main component for smaller screen devices

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -3,10 +3,16 @@ import styled from "styled-components";
 export const MainContainer = styled.main`
     max-width: 47.625rem;
     width: 100%;
-    margin: 9rem auto 0;
+    margin: 9rem auto 3rem;
     padding: 0 2rem;
 
     @media (max-width: 450px) {
         margin-top: 2rem;
+        margin-bottom: 2rem;
+    }
+
+    @media (max-width: 768px) {
+        margin-top: 4rem;
+        margin-bottom: 2rem;
     }
 `
