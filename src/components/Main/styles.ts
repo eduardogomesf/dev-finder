f2879f324bc0c0f26dd8ab41bba83b35c4ae03d9
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-11-27 13:38:12 -0300

    feat: make Main component responsive

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,8 +1,12 @@
 import styled from "styled-components";
 
 export const MainContainer = styled.main`
-    max-width: 762px;
+    max-width: 47.625rem;
     width: 100%;
     margin: 9rem auto 0;
     padding: 0 2rem;
+
+    @media (max-width: 450px) {
+        margin-top: 2rem;
+    }
 `
